e3504af0e64db37eb868c14004f53de85ae7c78c
suyeon <moa0823@naver.com>
2024-06-22 17:20:41 +0900

    naver meta tag #1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "내가 키우는 돼지",
   description: "내가 키우는 돼지 이름은 신채돌",
+  verification: {
+    other: {
+      "naver-site-verification": "de080567b31e95b10d99848b0a8e8370074dd733",
+    },
+  },
 };
 
 export default function RootLayout({
