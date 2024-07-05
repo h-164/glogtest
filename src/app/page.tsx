0387e09acf84fa39ce23573aa639163d8b572665
suyeon <moa0823@naver.com>
2024-07-05 17:12:13 +0900

    feat : write 페이지 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
-      <h1>신채연</h1>
+      <h1>신채돌</h1>
+      <h1>채ehf</h1>
+      <Link href="/write">채co</Link>
     </main>
   );
 }
---
+++ src/app/write/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function write() {
+  return (
+    <main>
+      <h1>write</h1>
+      <Link href="/">채co</Link>
+    </main>
+  );
+}
